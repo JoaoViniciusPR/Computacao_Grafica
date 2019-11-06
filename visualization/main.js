303ec7cf3c0ec4a8123eae9628672314cd7673d3
JoaoViniciusPR <j.joaoviniciuspr@hotmail.com>
2019-11-06 07:33:30 -0300

    Arrumando botao de animacao

--- visualization/main.js
+++ visualization/main.js
@@ -1,16 +1,20 @@
 var mainSVG = document.getElementById("svg");
 var width = mainSVG.width.animVal.value;
 var height = mainSVG.height.animVal.value;
-var number = document.getElementById("number");
-var prob = document.getElementById("prob");
-var anim = document.getElementById("anim");
 
 var st = create("rect",{id:"st", x:"0", y:"0", width:width, height:height, fill:"transparent", stroke:"black", "stroke-width":3});
 mainSVG.appendChild(st);
 
 var graph = new Graph("svg");
+
 function newRandomGraph(){
-    graph.random(number.value,prob.value,anim.value);
+	var number = document.getElementById("number");
+	var prob = document.getElementById("prob");
+	var anim = document.getElementById("anim");
+	print(anim);
+	print(anim.value);
+	var b = (anim.value === "true");
+    graph.random(number.value,prob.value,b);
 }
 
 $("body").keydown(function (evt){
